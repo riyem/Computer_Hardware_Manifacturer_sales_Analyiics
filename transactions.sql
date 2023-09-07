711cbb45041c66976c4b3359c4627a21ce0d620f
riyem <80780949+riyem@users.noreply.github.com>
2023-09-07 19:17:53 +0100

    Add files via upload

---
+++ transactions.sql
@@ -0,0 +1,2 @@
+SELECT sales.transactions.* , sales.date.* from sales.transactions inner join sales.date on sales.transactions.order_date=sales.date.date where sales.date.year=2020; 
+SELECT sum(sales.transactions.sales_amount) from sales.transactions inner join sales.date on sales.transactions.order_date=sales.date.date where sales.date.year=2020 and sales.transactions.market_code='Mark001'; /*revenue in chennai*/ 
